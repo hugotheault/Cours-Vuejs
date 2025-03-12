c014db64181717793739b2070f4fbb38961bd310
Hugo Theault <hugo.theault.1@gmail.com>
2025-03-12 10:54:12 +0100

    fin du td

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,5 +1,9 @@
 <script setup>
-import { TrashIcon, HandThumbUpIcon } from "@heroicons/vue/24/outline";
+import { TrashIcon, HandThumbUpIcon} from "@heroicons/vue/24/outline";
+import { HandThumbUpIcon as SolidHandThumbUpIcon } from "@heroicons/vue/24/solid";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
 
     const props = defineProps({
         post: {
@@ -18,24 +22,30 @@ import { TrashIcon, HandThumbUpIcon } from "@heroicons/vue/24/outline";
     function emitLike(){
         emit("like", props.post.id);
     }
+
+    function goToUserProfile(){
+        router.push({name: "user", params: {username: props.post.author.username}});
+    }
+
 </script>
 
 <template>
     <article class="card">
         <header>
           <img :src="post.author.avatarUrl" alt="avatar" width="36" height="36" class="avatar">
-          <a>{{ post.author.username }}</a>
+          <a @click="goToUserProfile" >{{ post.author.username }}</a>
         </header>
 
         <p >{{ post.content }}</p>
 
         <footer>
-            <button @click="deletePost" class="btn-icon">
-                <TrashIcon />
-            </button>
-            <a>{{ post.liked }}</a>
             <button @click="emitLike" class="btn-icon">
-                <HandThumbUpIcon />
+            <!--<HandThumbUpIcon v-if="!post.liked" />
+                <SolidHandThumbUpIcon v-else class="active" />-->
+                <component :is="post.liked ? SolidHandThumbUpIcon : HandThumbUpIcon" :class="{active : post.liked}"/>
+            </button>
+            <button @click="deletePost" class="btn-icon" id="trash">
+                <TrashIcon />
             </button>
         </footer>
       </article>
---
+++ src/pages/UserProfile.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const title = "User Page";
+const username = route.params.username;
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+    <h2>{{ $route.params.username }}</h2>
+<!--<h2> {{ username }}</h2> c'est la même chose -->
+</template>
